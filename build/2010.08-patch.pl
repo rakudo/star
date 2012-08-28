bb733c2f7b33f120bca60c497a202413d031800c
pmichaud <pmichaud@pobox.com>
2012-08-28 01:12:53 -0500

    Remove obsolete build/ directory.

--- build/2010.08-patch.pl
+++
@@ -1,9 +0,0 @@
-#! perl
-
-## Star patches to be applied to the 2010.08 release
-
-my $DISTDIR = $ENV{'DISTDIR'};
-
-system("cp build/patch/2010.08-3a339e.patch $DISTDIR/build");
-system("cd $DISTDIR/rakudo; patch -p1 <../build/2010.08-3a339e.patch");
-
