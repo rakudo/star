900c0c422b951f3eefcda677b5fdcb2b2056f62e
pmichaud <pmichaud@pobox.com>
2012-08-29 07:12:49 -0500

    Add script to run module tests.

---
+++ tools/build/modules-test.pl
@@ -0,0 +1,22 @@
+#! perl
+
+use Cwd;
+
+my $base = shift @ARGV;
+my $perl6 = shift @ARGV;
+
+while (<>) {
+    next if /^\s*(#|$)/;
+    my ($moduledir) = /(\S+)/;
+    print "Testing modules/$moduledir...\n";
+    if (-d "$base/modules/$moduledir/t") {
+        chdir("$base/modules/$moduledir");
+        system('prove', '-e', $perl6, '-r', 't');
+    }
+    else {
+        print "...no t/ directory found.\n";
+    }
+    print "\n";
+}
+
+0;
