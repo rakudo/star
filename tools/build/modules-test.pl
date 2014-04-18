a6cac5b692fd7ca32ca0ebd18ac867a3afc4d049
Moritz Lenz <moritz@faui2k3.org>
2014-04-18 18:42:42 +0200

    Include $perl6 in testing output

--- tools/build/modules-test.pl
+++ tools/build/modules-test.pl
@@ -8,7 +8,7 @@ my $perl6 = shift @ARGV;
 while (<>) {
     next if /^\s*(#|$)/;
     my ($moduledir) = /(\S+)/;
-    print "Testing modules/$moduledir...\n";
+    print "Testing modules/$moduledir with $perl6...\n";
     if (-d "$base/modules/$moduledir/t") {
         chdir("$base/modules/$moduledir");
         system('prove', '-e', $perl6, '-r', 't');
