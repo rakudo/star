f0def6dc7b4f576668e689cfd44745e506a305ad
Jens Rehsack <sno@netbsd.org>
2015-10-22 21:21:42 +0200

    introduce support for sdkroot and sysroot to support cross-builds
    To allow building for another location rather the final install prefix,
    regardless whether chroot based builds, cross-compile or canadian cross sdk
    is the goal, realize the difference between prefix, the sdk-root and the
    system root for final target.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- Configure.pl
+++ Configure.pl
@@ -32,6 +32,7 @@ MAIN: {
 
     my %options;
     GetOptions(\%options, 'help!', 'prefix=s',
+               'sysroot=s', 'sdkroot=s',
                'backends=s', 'no-clean!',
                'gen-nqp:s', 'gen-moar:s',
                'make-install!', 'makefile-timing!',
@@ -59,9 +60,9 @@ MAIN: {
     }
 
     unless (defined $options{prefix}) {
-        my $dir = getcwd;
-        print "ATTENTION: no --prefix supplied, building and installing to $dir/install\n";
-        $options{prefix} = 'install';
+	my $default = defined($options{sysroot}) ? '/usr' : File::Spec->catdir(getcwd, 'install');
+        print "ATTENTION: no --prefix supplied, building and installing to $default\n";
+        $options{prefix} = $default;
     }
     $options{prefix} = File::Spec->rel2abs($options{prefix});
 
@@ -148,6 +149,8 @@ MAIN: {
     }
 
     $config{prefix} = $prefix;
+    $config{sdkroot} = $options{sdkroot};
+    $config{sysroot} = $options{sysroot};
     $config{slash}  = $slash;
     $config{'makefile-timing'} = $options{'makefile-timing'};
     $config{'stagestats'} = '--stagestats' if $options{'makefile-timing'};
@@ -263,6 +266,9 @@ Configure.pl - $lang Configure
 General Options:
     --help             Show this text
     --prefix=dir       Install files in dir; also look for executables there
+    --sdkroot=dir      When given, use for searching build tools here, e.g.
+                       nqp, java etc.
+    --sysroot=dir      When given, use for searching runtime components here
     --backends=jvm,moar
                        Which backend(s) to use
     --gen-moar[=branch]
