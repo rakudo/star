226eb20032baac7c4bda074e0d16b741ecbf568a
Tobias Leich <email@froggs.de>
2016-01-27 20:45:04 +0100

    revert using --bindir when installing panda
    We can install panda and the binaries of the Star dist into install/bin,
    though when the user later installs binaries or updates panda, these would
    go into install/share/perl6/site/bin by default.
    Since there is currently no sane way to prevent that, we just install
    non-core stuff info [...]/site/bin, and the user will have to add two paths
    to PATH.

--- tools/build/module-install.pl
+++ tools/build/module-install.pl
@@ -4,14 +4,13 @@ use warnings;
 use strict;
 my $perl6bin   = shift @ARGV;
 my $pandabin   = shift @ARGV;
-my $bindir     = shift @ARGV;
 
 my $exit = 0;
 
 while (<>) {
     next if /^\s*(#|$)/;
     my ($module) = /(\S+)/;
-    $exit ||= system $perl6bin, $pandabin, '--force', "--bin-prefix=$bindir", "install", "modules/$module";
+    $exit ||= system $perl6bin, $pandabin, '--force', "install", "modules/$module";
 }
 
 exit $exit;
