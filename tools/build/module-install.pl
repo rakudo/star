2a4183659e70fec3d4d63cfbe48b9aed387adbdf
Patrick Spek <p.spek@tyil.nl>
2019-10-31 02:14:41 +0100

    Update zef command for installing bundled modules

--- tools/build/module-install.pl
+++ tools/build/module-install.pl
@@ -14,8 +14,6 @@ while (<>) {
     next if /^\s*(#|$)/;
     my ($module) = /(\S+)/;
     $exit ||= system $perl6bin, $zefbin,
-      '--/build-depends', '--/test-depends', '--/depends', 
-      '--/p6c', '--/metacpan', '--/cpan',
       '--force', '--/test', 'install', "./modules$path_sep$module";
 }
 
