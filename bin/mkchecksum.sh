8eefd1c44de429f7f28815efadd5a57910d3757f
Patrick Spek <p.spek@tyil.nl>
2019-11-01 11:49:54 +0100

    Use the superiour indentation method

--- bin/mkchecksum.sh
+++ bin/mkchecksum.sh
@@ -2,23 +2,23 @@
 
 main()
 {
-    if [ -z "$1" ]
-    then
-        usage
-        exit 1
-    fi
+	if [ -z "$1" ]
+	then
+		usage
+		exit 1
+	fi
 
-    printf "md5    %s\n" "$(md5sum "$1" | cut -f1 -d" ")"
-    printf "sha1   %s\n" "$(sha1sum "$1" | cut -f1 -d" ")"
-    printf "sha224 %s\n" "$(sha224sum "$1" | cut -f1 -d" ")"
-    printf "sha256 %s\n" "$(sha256sum "$1" | cut -f1 -d" ")"
-    printf "sha384 %s\n" "$(sha384sum "$1" | cut -f1 -d" ")"
-    printf "sha512 %s\n" "$(sha512sum "$1" | cut -f1 -d" ")"
+	printf "md5    %s\n" "$(md5sum "$1" | cut -f1 -d" ")"
+	printf "sha1   %s\n" "$(sha1sum "$1" | cut -f1 -d" ")"
+	printf "sha224 %s\n" "$(sha224sum "$1" | cut -f1 -d" ")"
+	printf "sha256 %s\n" "$(sha256sum "$1" | cut -f1 -d" ")"
+	printf "sha384 %s\n" "$(sha384sum "$1" | cut -f1 -d" ")"
+	printf "sha512 %s\n" "$(sha512sum "$1" | cut -f1 -d" ")"
 }
 
 usage()
 {
-    printf "NYI\n"
+	printf "NYI\n"
 }
 
 main "$@"
